8888328ef690827e7623315d1b81e4c3e1312410
nickonyi <nickoluoch1130@gmail.com>
2023-06-11 09:01:47 +0300

    edit project functionality

--- src/editingProject.js
+++ src/editingProject.js
@@ -3,6 +3,10 @@ import { updateTitle } from "./creatingTask"
 
 function editContainerEventListener() {
     document.addEventListener("click", showDropDown);
+
+    const option = document.querySelector('.project .option');
+    option.firstElementChild.addEventListener('click', showRenameForm);
+    option.lastElementChild.addEventListener('click', deleteProject);
 }
 
 function showDropDown(e) {
@@ -28,16 +32,38 @@ function showDropDown(e) {
     });
 }
 
-function showRenameForm() {
+function showRenameForm(e) {
+    let editContainerNode = e.target.parentNode.parentNode;
+    let tileNode = editContainerNode.parentNode;
 
+    hideDropDown(editContainerNode);
+    let haveForm = checkFormExist();
+    if (haveForm === true) {
+        revertRenameFormLocation();
+        displayRenamedProject();
+    }
+
+    relocateRenameForm(tileNode);
+    animateRenameForm();
+
+    document.querySelector('.project-rename-input').focus();
 }
 
-function hideDropDown() {
-
+function hideDropDown(editContainerNode) {
+    editContainerNode.classList.remove('active');
 }
 
-function deleteProject() {
+function deleteProject(e) {
+    let tile = e.target.closest('.tile');
+    let index = tile.dataset.project;
 
+    if (tile.classList.contains('selected')) {
+        const today = document.querySelector("#today");
+        const nameNode = today.querySelector("[data-name]");
+
+        today.classList.add("selected");
+        updateTitle(nameNode);
+    }
 }
 
 function revertOptionLocation() {
@@ -65,6 +91,51 @@ function relocateOption(e) {
         editContainer.appendChild(listOption);
     }
 
+}
+
+function checkFormExist() {
+    const renameForm = document.querySelector('#rename-form');
+
+    if (renameForm.classList.contains('hidden')) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//revert form back to its original posting under .project
+function revertRenameFormLocation() {
+    const renameForm = document.getElementById("rename-form");
+    const project = document.querySelector(".project");
+
+    renameForm.classList.add("hidden");
+    project.appendChild(renameForm);
+}
+
+function relocateRenameForm(tileNode) {
+    const projectNode = tileNode.parentNode;
+    const renameForm = document.getElementById('rename-form');
+
+    const nameNode = tileNode.querySelector('.project-name');
+    let name = nameNode.textContent;
+
+    const input = renameForm.querySelector('input');
+    input.value = name;
+
+    projectNode.insertBefore(renameForm, tileNode);
+}
+
+function animateRenameForm() {
+    const form = document.querySelector('#rename-form');
+
+    setTimeout(() => {
+        form.classList.remove('hidden');
+    }, 0);
+}
+
+function displayRenamedProject() {
+    const hiddenTile = document.querySelector('div.hidden');
+    hiddenTile.classList.remove('hidden');
 }
 
 function sortArray() {
